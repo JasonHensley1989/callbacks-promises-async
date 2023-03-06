75586cf567e7b6f82a5bbfd2f6c7d25e6e649c44
Jason Hensley <jasonhensley1989@gmail.com>
2023-03-06 16:16:22 -0600

    set up for callbacks

--- callbacks.js
+++ callbacks.js
@@ -3,4 +3,14 @@ console.log("Callbacks prepared");
 const posts = [
     { title: "Post One", body:"this is post one"},
     { title: "Post Two", body:"this is post two"}
-]
+];
+
+function getPosts() {
+    setTimeout(() => {
+    let output = "";
+    posts.forEach((post, index) => {
+        output += `<li>${post.title}</li>`;
+    })
+    }, 1000)
+    document.body.innerHTML = output;
+}
