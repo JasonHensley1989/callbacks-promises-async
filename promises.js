ad7de14f408ba984997cf45857f32afc2d81d82a
Jason Hensley <jasonhensley1989@gmail.com>
2023-03-06 16:50:04 -0600

    promises basic layour

---
+++ promises.js
@@ -0,0 +1,26 @@
+const posts = [
+    { title: "Post One", body:"this is post one"},
+    { title: "Post Two", body:"this is post two"}
+];
+
+// this mimics fetching from a server.
+function getPosts() {
+    setTimeout(() => {
+    let output = "";
+    posts.forEach((post, index) => {
+        output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+    }, 1000)
+}    
+
+// this function mimics a create post which posts to the post array
+// because the timeout is longer than the getPosts the DOM is already
+// finished executing, therefore a callback is needed to push the post
+// before the DOM is executed, the callback 
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000)
+}
