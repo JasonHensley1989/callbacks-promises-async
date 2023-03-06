1aa9fc2ad7de31adaa70e27b935918409be37643
Jason Hensley <jasonhensley1989@gmail.com>
2023-03-06 17:17:30 -0600

    promises, promise.all

--- promises.js
+++ promises.js
@@ -14,13 +14,38 @@ function getPosts() {
     }, 1000)
 }    
 
-// this function mimics a create post which posts to the post array
-// because the timeout is longer than the getPosts the DOM is already
-// finished executing, therefore a callback is needed to push the post
-// before the DOM is executed, the callback 
-function createPost(post, callback) {
-    setTimeout(() => {
-        posts.push(post);
-        callback();
-    }, 2000)
+// this function mimics a create fetch from a server but uses promises instead of callback functions
+// the code purposely uses a error to show the necessity of a catch but switching it to the reverse performs
+// the same function as a resolve.
+
+function createPost(post) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+            
+            const error = true;
+
+            if(!error) {
+                resolve();
+            } else {
+                reject("Error: Something went wrong")
+            }
+        }, 2000)
+    });
 }
+
+
+// createPost({ title: "Post Three", body: "new post body"})
+//     .then(getPosts)
+//     .catch(err => console.log(err))
+
+// Promise.all
+const promise1 = Promise.resolve("Hello World");
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000, 'Goodbye')
+});
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+
+
+Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values))
